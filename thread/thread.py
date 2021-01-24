641fc3f8aa1b18b61901359f19bb122bf70d52be
tonerqq <sypnowichp.backup@gmail.com>
2021-01-23 23:58:25 -0500

    completed 4chan thread downloader

--- thread/thread.py
+++ thread/thread.py
@@ -3,21 +3,21 @@
 # @date     23 Jan 2021
 
 import re
-import os 
-
-# 1609214554461.jpg -- 13 numbers
+import subprocess as sp
+import urllib
 
 # get thread link from user and download source code
 thread_link = raw_input(" ENTER 4CHAN THREAD LINK : ")
-page_code = os.system("curl " + str(thread_link))
+
+# download thread source code
+page_code = sp.check_output("curl " + str(thread_link), shell=True)
 
 # find all numbers in source code
 raw_nums = re.findall('[0-9]+', str(page_code))
-num_list = []
-# find all desirable JPG numbers
+raw_nums = set(raw_nums)
+# find all desirable JPG numbers and download them
 for i in raw_nums:
-    if (len(str(i)) == 13):
-        num_list.append(i)
-        print i
-
-
+    if (len(str(i)) == 13 and str(i)[1] == "6"):
+        url = "https://i.4cdn.org/wg/" + str(i) + ".jpg"
+        print(" DOWNLOADING " + url + " ...")
+        urllib.urlretrieve(url, (str(i) + ".jpg"))
